b9fc874a5b0b9e814fc79800aa241ee96dced8fb
DylanSteurs <144011463+DylanSteurs@users.noreply.github.com>
2024-03-18 10:49:24 +0100

    Commit 2
    Stap 1 afgemaakt

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,19 @@
+public enum Path
+{
+    Path1,
+    Path2
+
+}
+public enum TowerType
+{
+    Archer,
+    Sword,
+    Wizard
+}
+public enum SiteLevel
+{
+    Onbebouwd,
+    lvl1,
+    lvl2,
+    lvl3,
+}
